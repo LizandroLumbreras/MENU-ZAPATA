06131be4028feec19b3b1f17bff4c76d5fa08596
LizandroLumbreras <lizandrodejesus0@gmail.com>
2025-07-02 16:26:27 -0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('menu-zapata-cache').then(function(cache) {
+      return cache.addAll([
+        'menu.html',
+        'manifest.json',
+        'logo_proveedora.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
